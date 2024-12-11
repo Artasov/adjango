8274de264fcadddc21477017d1826133136f0be4
Artasov <ivanhvalevskey@gmail.com>
2024-12-11 14:49:28 +0300

    v0.2.8

--- adjango/conf.py
+++ adjango/conf.py
@@ -2,19 +2,25 @@
 from django.conf import settings
 
 
-def get_setting(setting_name, default):
-    return getattr(settings, setting_name, default)
+def get_setting(name, default=None, required=False):
+    value = getattr(settings, name, None)
+    if value is None:
+        if required:
+            if default is not None:
+                return default
+            raise ValueError(f'Missing required django setting: {name}')
+        return default
+    return value
 
 
 ADJANGO_BACKENDS_APPS = get_setting('ADJANGO_BACKENDS_APPS', settings.BASE_DIR)
 ADJANGO_FRONTEND_APPS = get_setting('ADJANGO_FRONTEND_APPS', settings.BASE_DIR)
-ADJANGO_APPS_PREPATH = get_setting('ADJANGO_APPS_PREPATH', None)
+ADJANGO_APPS_PREPATH = get_setting('ADJANGO_APPS_PREPATH')
 ADJANGO_UNCAUGHT_EXCEPTION_HANDLING_FUNCTION = get_setting(
     'ADJANGO_UNCAUGHT_EXCEPTION_HANDLING_FUNCTION',
-    None
 )
 ADJANGO_CONTROLLERS_LOGGER_NAME = get_setting('ADJANGO_CONTROLLERS_LOGGER_NAME', 'global')
-ADJANGO_CONTROLLERS_LOGGING = get_setting('ADJANGO_CONTROLLERS_LOGGING', False)
+ADJANGO_CONTROLLERS_LOGGING = get_setting('ADJANGO_CONTROLLERS_LOGGING',)
 ADJANGO_EMAIL_LOGGER_NAME = get_setting('ADJANGO_EMAIL_LOGGER_NAME', 'email')
-ADJANGO_IP_LOGGER = get_setting('ADJANGO_IP_LOGGER', None)
-ADJANGO_IP_META_NAME = get_setting('ADJANGO_IP_META_NAME', None)
+ADJANGO_IP_LOGGER = get_setting('ADJANGO_IP_LOGGER')
+ADJANGO_IP_META_NAME = get_setting('ADJANGO_IP_META_NAME')
