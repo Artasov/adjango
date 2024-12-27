8c2af82ed3f49c26f4f8e13451032ac21fbb2172
Artasov <ivanhvalevskey@gmail.com>
2024-12-28 02:34:10 +0300

    add AsyncTestAPIClient

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="adjango",
-    version="0.3.4",
+    version="0.3.5",
     author="xlartas",
     author_email="ivanhvalevskey@gmail.com",
     description="A library with many features for interacting with Django",
