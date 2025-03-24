32077f0a026336e558e46327210ff777db98e09b
Artasov <ivanhvalevskey@gmail.com>
2025-03-24 17:58:07 +0300

    feat: v0.4.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="adjango",
-    version="0.4.7",
+    version="0.4.8",
     author="xlartas",
     author_email="ivanhvalevskey@gmail.com",
     description="A library with many features for interacting with Django",
